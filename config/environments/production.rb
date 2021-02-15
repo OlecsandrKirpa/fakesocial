f44190c238c4f44501cc9dcee3d5e6aad27df921
Sasha <olecsandr.kirpachov@gmail.com>
2021-02-15 16:27:50 +0100

    devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # ACTUAL HOSTING
+config.action_mailer.default_url_options = { host: 'https://fakesocial.herokuapp.com/', port: 3000 }
 end
