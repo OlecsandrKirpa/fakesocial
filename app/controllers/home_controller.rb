e65fa0d930b19bba392c75684845f92c7d2ef04b
Sasha <olecsandr.kirpachov@gmail.com>
2021-02-16 16:01:47 +0100

    Get client ip test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
 class HomeController < ApplicationController
   def index
 
-    @ip = "I'm sorry, there is an error." 
+    @ip = " I'm sorry, there is an error. " 
 
     begin
    @ip = request.remote_ip
-rescue => exception
+    rescue => exception
    @ip = except.to_s
-end
+    end
 
     time = Time.now
     
@@ -18,6 +18,8 @@ end
     hour = time.strftime("%k:%M")
     @date = "Today's date: "+day.to_s + "-" +month.to_s + "-" + year.to_s
     @hours = "It's "+hour.to_s
+
+
   end
 
   def about
